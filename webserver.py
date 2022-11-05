45ee70d3fb79c51fc30d60a0f694cec3827d4c7f
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-11-05 10:58:48 +0530

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -1,22 +1,28 @@
-from flask import Flask
+from flask import Flask ,render_template
   
  
 app = Flask(__name__) 
+
   
   
 @app.route('/addnumber/<int:num1>/<int:num2>') 
 def add(num1,num2): 
-     return f'ans is: str{num1+num2}'
+     return f'ans is: {num1+num2}'
 
 @app.route('/subnumber/<int:num1>/<int:num2>') 
 def sub(num1,num2): 
-     return f'ans is: str{num1-num2}'
+     return f'ans is: {num1-num2}'
  
 @app.route('/mulnumber/<int:num1>/<int:num2>') 
 def mul(num1,num2): 
-     return f'ans is: str{num1*num2}'
+     return f'ans is: {num1*num2}'
 
+@app.route('/home') 
+def home(): 
+     return render_template('arthmetic.html') 
   
 
   
-app.run()
+
+  
+app.run(host='0.0.0.0')
